e07cfdb955726535da8476502936bea6eda61fa5
临时账户 <human2@HumandeMacBook-Pro.local>
2021-08-07 17:30:32 +0800

    commit test file

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div className='flex items-center justify-center h-screen bg-gray-100'>
+        </div>
+    )
+}
+export default App
